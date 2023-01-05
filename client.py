90334fc2ec5d03138bb2c129479f1cb4535e8732
maciejkawka <mkawka100@gmail.com>
2023-01-05 23:20:55 +0100

    Dodanie testowego serwera i klienta

--- client.py
+++ client.py
@@ -6,16 +6,41 @@ import pickle
 import struct
 import imutils
 
-def create_socket(server_address, server_port):
+HOST = '127.0.0.1'
+PORT = 5001
+
+def create_socket():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((server_address, server_port))
     return client_socket
 
-def getting_server_connect():
-    pass
+def getting_server_connect(client_socket, server_address, server_port):
+    data = b""
+    #client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((server_address, server_port))
+    payload_size = struct.calcsize("Q")
+    while True:
+        while len(data) < payload_size:
+            packet = client_socket.recv(4096)
+            if not packet: break
+            data+=packet
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack("Q",packed_msg_size)[0]
+        while len(data) < msg_size:
+            data += client_socket.recv(4096)
+        frame_data = data[:msg_size]
+        data = data[msg_size:]
+        frame = pickle.loads(frame_data)
+        cv2.imshow("Receiving...", frame)
+        key = cv2.waitKey(10)
+        if key == 13:
+            break
+    client_socket.close()
+
 
 def testClient():
-    pass
+    client_socket = create_socket()
+    getting_server_connect(client_socket, HOST, PORT)
 
 if __name__ == "__main__":
     testClient()
--- server.py
+++ server.py
@@ -6,21 +6,35 @@ import pickle
 import struct
 import imutils
 
-HOST = "127.0.0.1"
-PORT = 5000
+HOST = '127.0.0.1'
+PORT = 5001
 
 def create_socket(server_address, server_port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host_name = socket.gethostname()
-    socket_address = (server_address, server_port)
+    socket_address = ((HOST, PORT))
     server_socket.bind(socket_address)
     return server_socket
 
-def accepting_connection():
-    pass
+def accepting_connection(server_socket):
+    while True:
+        server_socket.listen()
+        client, addr = server_socket.accept()
+        if client:
+            vid = cv2.VideoCapture(0)
+            while(vid.isOpened()):
+                img, frame = vid.read()
+                a = pickle.dumps(frame)
+                message = struct.pack("Q", len(a))+a
+                client.sendall(message)
+                cv2.imshow('Sending...', frame)
+                key = cv2.waitKey(10)
+
 
 def testServer():
-    pass
+    server_socket = create_socket(socket.gethostbyname(socket.gethostname()), PORT)
+    accepting_connection(server_socket)
 
 if __name__ == "__main__":
     testServer()
+    #print(str(socket.gethostbyname(socket.gethostname())))
