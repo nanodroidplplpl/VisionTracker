7831ddb6e4f6b414d015f3cdb9831d8444c5a704
maciejkawka <mkawka100@gmail.com>
2023-01-06 20:14:16 +0100

    Dodanie pierwszych czterech kroków synchronicznego połączenia pomiędzy klient i serwer. Te kroki to: connect, tytuły, wybrany tytuł i mind point.

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 # Modol klienta odbierajacy stream od serwera, ktory
 # jednoczesnie wysyla informacje o tym gdzie zwiekszyc rozdzielczosc
 import socket
+from time import sleep
+
 import cv2
 import pickle
 import struct
@@ -8,39 +10,92 @@ import imutils
 
 HOST = '127.0.0.1'
 PORT = 5001
-
-def create_socket():
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    return client_socket
-
-def getting_server_connect(client_socket, server_address, server_port):
-    data = b""
-    #client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((server_address, server_port))
-    payload_size = struct.calcsize("Q")
-    while True:
-        while len(data) < payload_size:
-            packet = client_socket.recv(4096)
-            if not packet: break
-            data+=packet
-        packed_msg_size = data[:payload_size]
-        data = data[payload_size:]
-        msg_size = struct.unpack("Q",packed_msg_size)[0]
-        while len(data) < msg_size:
-            data += client_socket.recv(4096)
-        frame_data = data[:msg_size]
-        data = data[msg_size:]
-        frame = pickle.loads(frame_data)
-        cv2.imshow("Receiving...", frame)
-        key = cv2.waitKey(10)
-        if key == 13:
-            break
-    client_socket.close()
+PORT_CLIENT = 5002
 
 
-def testClient():
-    client_socket = create_socket()
-    getting_server_connect(client_socket, HOST, PORT)
+class Client():
+    client_socket = None
+    server_socket = None
+    client_server_socket = None #adres kliena jako serweru
+    server_client_socket = None # adres serweru jako klienta
+
+    def __init__(self, host, port, port_client):
+        self.HOST = host
+        self.PORT = port
+        self.PORT_CLIENT = port_client
+
+    def create_socket(self):
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        return client_socket
+
+    def getting_server_connect(self, client_socket, server_address, server_port):
+        data = b""
+        # client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((server_address, server_port))
+        payload_size = struct.calcsize("Q")
+        while True:
+            while len(data) < payload_size:
+                packet = client_socket.recv(4096)
+                if not packet: break
+                data += packet
+            packed_msg_size = data[:payload_size]
+            data = data[payload_size:]
+            msg_size = struct.unpack("Q", packed_msg_size)[0]
+            while len(data) < msg_size:
+                data += client_socket.recv(4096)
+            frame_data = data[:msg_size]
+            data = data[msg_size:]
+            frame = pickle.loads(frame_data)
+            cv2.imshow("Receiving...", frame)
+            key = cv2.waitKey(10)
+            if key == 13:
+                break
+        client_socket.close()
+
+    def brackets_out(self, data):
+        data = data[:len(data) - 1]
+        data = data[1:]
+        return data
+    def get_titles(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((self.HOST, self.PORT))
+        data = self.client_socket.recv(4096).decode()
+        print(data)
+        data = self.brackets_out(data)
+        print(data)
+        data = data.split(', ')
+        i = 0
+        for d in data:
+            data[i] = self.brackets_out(d)
+            i += 1
+            #print(d)
+        print(data)
+        return data
+
+    def send_chosen_title(self, choice, data):
+        self.client_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host_name = socket.gethostname()
+        client_server_address = ((self.HOST, self.PORT_CLIENT))
+        self.client_server_socket.bind(client_server_address)
+        while True:
+            self.client_server_socket.listen()
+            server_client_socket, addr = self.client_server_socket.accept()
+            if server_client_socket:
+                server_client_socket.sendall(data[choice].encode())
+                # do wywalenia
+                #self.server_socket.close()
+                return
+
+    def wyslanie_mindpoint(self, mindpoint):
+        pass
+    def testClient(self):
+        client_socket = self.create_socket()
+        self.getting_server_connect(client_socket, HOST, PORT)
+
 
 if __name__ == "__main__":
-    testClient()
+    client = Client(HOST, PORT, PORT_CLIENT)
+    data = client.get_titles()
+    #data = ['taniec_na_lodzie.mp4', 'taniec_na_sniegu.mp4']
+    client.send_chosen_title(0, data)
+    #client.testClient()
--- server.py
+++ server.py
@@ -1,40 +1,104 @@
 # Modol servera, ktory streamoje film do polaczonego klienta
 # Odbierajac przy tym informacje o tym gdzie zwiekszyc rozdzielczosc
 import socket
+from time import sleep
+
 import cv2
 import pickle
 import struct
 import imutils
+import os
 
 HOST = '127.0.0.1'
 PORT = 5001
 
-def create_socket(server_address, server_port):
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host_name = socket.gethostname()
-    socket_address = ((HOST, PORT))
-    server_socket.bind(socket_address)
-    return server_socket
 
-def accepting_connection(server_socket):
-    while True:
-        server_socket.listen()
-        client, addr = server_socket.accept()
-        if client:
-            vid = cv2.VideoCapture(0)
-            while(vid.isOpened()):
-                img, frame = vid.read()
-                a = pickle.dumps(frame)
-                message = struct.pack("Q", len(a))+a
-                client.sendall(message)
-                cv2.imshow('Sending...', frame)
-                key = cv2.waitKey(10)
+class Server():
+    client_socket = None
+    server_socket = None
+    client_server_socket = None
+    PORT_CLIENT_SERVER = 5002
 
+    def __init__(self, host, port):
+        self.clinet_server_socket = None
+        self.HOST = host
+        self.PORT = port
 
-def testServer():
-    server_socket = create_socket(socket.gethostbyname(socket.gethostname()), PORT)
-    accepting_connection(server_socket)
+    def create_socket(self, server_address, server_port):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host_name = socket.gethostname()
+        socket_address = ((HOST, PORT))
+        #self.server_socket = server_socket
+        server_socket.bind(socket_address)
+        return server_socket
+
+    def accepting_connection(self, server_socket):
+        while True:
+            server_socket.listen()
+            client, addr = server_socket.accept()
+            if client:
+                # vid = cv2.VideoCapture(0)
+                vid = cv2.VideoCapture("taniec_na_lodzie.mp4")
+                while (vid.isOpened()):
+                    img, frame = vid.read()
+                    a = pickle.dumps(frame)
+                    message = struct.pack("Q", len(a)) + a
+                    client.sendall(message)
+                    print("Wielkość wysyłanego pliku: " + str(len(a)))
+                    cv2.imshow('Sending...', frame)
+                    key = cv2.waitKey(10)
+
+    def get_titles(self):
+        files = os.listdir()
+        titles = []
+        for file in files:
+            if file.endswith('.mp4'):
+                titles.append(str(file))
+                print(file)
+        print(titles)
+        return titles
+
+# Step one of comunication
+    def send_titles(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host_name = socket.gethostname()
+        socket_address = ((self.HOST, self.PORT))
+        self.server_socket.bind(socket_address)
+        while True:
+            self.server_socket.listen()
+            self.client_socket, addr = self.server_socket.accept()
+            if self.client_socket:
+                titles = self.get_titles()
+                print(titles)
+                self.client_socket.sendall(str(titles).encode())
+                # do wywalenia
+                #self.server_socket.close()
+                return
+
+    def get_chosen_title(self):
+        sleep(0.5)
+        self.client_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #client_server_socket.settimeout(2)
+        while True:
+            try:
+                self.client_server_socket.connect((self.HOST, self.PORT_CLIENT_SERVER))
+                break
+            except socket.error as err:
+                sleep(0.5)
+                print("Wyjatek: " + str(err))
+        data = self.client_server_socket.recv(4096).decode()
+        print(data)
+        return data
+
+    def testServer(self):
+        server_socket = self.create_socket(socket.gethostbyname(socket.gethostname()), PORT)
+        self.accepting_connection(server_socket)
+
 
 if __name__ == "__main__":
-    testServer()
-    #print(str(socket.gethostbyname(socket.gethostname())))
+    server = Server(HOST, PORT)
+    #server.testServer()
+    server.send_titles()
+    #sleep(0.5)
+    server.get_chosen_title()
+    # print(str(socket.gethostbyname(socket.gethostname())))
